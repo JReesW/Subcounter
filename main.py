47411380ba5d8a21c4a9d75b98e01ebb00e2f3bc
JReesW <j.rees.w@gmail.com>
2022-06-15 21:00:48 +0200

    refactored

--- main.py
+++ main.py
@@ -18,37 +18,31 @@ con = sqlite3.connect("database.db")
 cur = con.cursor()
 
 
-"""
-DATABASE:
-
-table records
-    int year
-    int month
-    int day
-    int subs
-"""
+sub_ids = {
+    935553013710483546: "minecraft_survival",
+    935553034963001345: "mc_survival"
+}
 
 
 class Subcounter(discord.Client):
     def __init__(self):
         super().__init__()
-        self.minecraft_survival = None
-        self.mc_survival = None
 
-    async def on_ready(self):
+    @staticmethod
+    async def on_ready():
         print(f"{client.user} has connected to Discord!")
-        self.minecraft_survival = await reddit.subreddit("minecraft_survival", fetch=True)  # 935553013710483546
-        self.mc_survival = await reddit.subreddit("mc_survival", fetch=True)  # 935553034963001345
 
-    async def on_message(self, message):
-        if message.channel.id == 935553013710483546:
-            # Minecraft_Survival
+    @staticmethod
+    async def on_message(message):
+        if (m_id := message.channel.id) in sub_ids:
             if message.content.lower() == "chart":
-                year, month, day = (int(x) for x in dt.datetime.today().strftime('%Y-%m-%d').split("-"))
-                subs = self.minecraft_survival.subscribers
-                active = self.minecraft_survival.active_user_count
+                subreddit = await reddit.subreddit(sub_ids[m_id], fetch=True)  # 935553013710483546
 
-                cur.execute("SELECT * FROM records")
+                year, month, day = (int(x) for x in dt.datetime.today().strftime('%Y-%m-%d').split("-"))
+                subs = subreddit.subscribers
+                active = subreddit.active_user_count
+
+                cur.execute(f"SELECT * FROM {sub_ids[m_id]}")
                 rows = cur.fetchall()
 
                 for y, m, d, s in rows:
@@ -57,14 +51,14 @@ class Subcounter(discord.Client):
                             break
                         else:
                             cur.execute(f"""
-                                UPDATE records SET subs = {s} WHERE year = {year} AND month = {month} AND day = {day}
+                                UPDATE {sub_ids[m_id]} SET subs = {s} WHERE year = {year} AND month = {month} AND day = {day}
                             """)
                             break
                 else:
-                    cur.execute(f"INSERT INTO records VALUES ({year}, {month}, {day}, {subs})")
+                    cur.execute(f"INSERT INTO {sub_ids[m_id]} VALUES ({year}, {month}, {day}, {subs})")
                 con.commit()
 
-                cur.execute("SELECT * FROM records")
+                cur.execute(f"SELECT * FROM {sub_ids[m_id]}")
                 rows = cur.fetchall()
 
                 # Parse data
@@ -85,7 +79,7 @@ class Subcounter(discord.Client):
                 data_stream.seek(0)
                 chart = discord.File(data_stream, filename="subs_chart.png")
 
-                embed = discord.Embed(title="Sub counts of the last 7 registries")
+                embed = discord.Embed(title=f"{sub_ids[m_id]} sub counts of the last 7 registries")
                 embed.add_field(name="Subs", value=str(subs), inline=True)
                 embed.add_field(name="Active", value=str(active), inline=True)
                 embed.set_image(url="attachment://subs_chart.png")
@@ -93,10 +87,6 @@ class Subcounter(discord.Client):
                 plt.clf()
 
                 await message.channel.send(embed=embed, file=chart)
-        elif message.channel.id == 935553034963001345:
-            # MC_Survival
-            if message.content.lower() == "stats":
-                await message.channel.send(self.mc_survival.subscribers)
         # if message.author.id == config.ADMIN_ID:
         #     await message.channel.send("Hello, John!")
 
--- setup_db.py
+++ setup_db.py
@@ -9,7 +9,11 @@ cur = con.cursor()
 cur.execute("DROP TABLE IF EXISTS records")
 
 cur.execute("""
-    CREATE TABLE records (year int, month int, day int, subs int)
+    CREATE TABLE minecraft_survival (year int, month int, day int, subs int)
+""")
+
+cur.execute("""
+    CREATE TABLE mc_survival (year int, month int, day int, subs int)
 """)
 
 
@@ -24,6 +28,24 @@ days = [
     (11, 3801),
     (13, 3810)
 ]
+
+days2 = [
+    (1, 1327),
+    (3, 1327),
+    (4, 1327),
+    (5, 1327),
+    (8, 1327),
+    (9, 1327),
+    (10, 1327),
+    (11, 1327),
+    (13, 1327),
+    (14, 1328)
+]
+
 for day, s in days:
-    cur.execute(f"INSERT INTO records VALUES (2022, 6, {day}, {s})")
+    cur.execute(f"INSERT INTO minecraft_survival VALUES (2022, 6, {day}, {s})")
+    con.commit()
+
+for day, s in days2:
+    cur.execute(f"INSERT INTO mc_survival VALUES (2022, 6, {day}, {s})")
     con.commit()
